2ab92f39f0a5d66f6e33406b78ba937af6531285
kennedy-anon <85305895+kennedy-anon@users.noreply.github.com>
2022-05-07 08:31:10 +0300

    An ecommerce rest api

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>console.log("Database connection successful")).catch((err)=>{
+    console.log(err)
+})
+
+/*
+app.get("/api/test", ()=>{
+    console.log("test is successful");
+});
+*/
+
+app.use(express.json());
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server is up and running");
+})
