9c70f328320e76958c26979b8e72e7a17ed05f19
kennedy-anon <85305895+kennedy-anon@users.noreply.github.com>
2022-05-09 23:30:42 +0300

    Implemented the user stats, products and cart apis.
    Stats, Functionality implemented
    Products, Add, get all, get one, delete, update
    Carts, create new, update cart details, delete, get one cart, get all carts(only admins)

--- routes/user.js
+++ routes/user.js
@@ -4,66 +4,80 @@ const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = requir
 const router = require("express").Router();
 
 //update user details
-router.put("/:id", verifyTokenAndAuthorization, async (req, res)=>{
-    if (req.body.password){
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
         req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString();
     }
 
-    try{
+    try {
         const updatedUser = await User.findByIdAndUpdate(req.params.id, {
             $set: req.body,
         }, { new: true });
         res.status(200).json(updatedUser);
-    }catch(err){
+    } catch (err) {
         res.status(500).json(err);
     }
 });
 
 //delete
-router.delete("/:id", verifyTokenAndAuthorization, async (req, res)=>{
-    try{
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
         await User.findByIdAndDelete(req.params.id);
         res.status(200).json("User deleted.");
-    }catch(err){
+    } catch (err) {
         res.status(500).json(err);
     }
 })
 
 // get a user
-router.get("/find/:id", verifyTokenAndAdmin, async (req, res)=>{
-    try{
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
         const user = await User.findById(req.params.id);
 
         const { password, ...others } = user._doc;
         res.status(200).json(others);
-    }catch(err){
+    } catch (err) {
         res.status(500).json(err);
     }
 })
 
 //get all users
-router.get("/", verifyTokenAndAdmin, async (req, res)=>{
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
     const query = req.query.new
-    try{
+    try {
         //if (query)
-        const users = query ? await User.find().sort({_id:-1}).limit(5) : await User.find();
+        const users = query ? await User.find().sort({ _id: -1 }).limit(5) : await User.find();
 
         res.status(200).json(users);
-    }catch(err){
+    } catch (err) {
         res.status(500).json(err);
     }
 })
 
 //get user stats
-router.get("/stats", verifyTokenAndAdmin, async(req, res)=>{
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
     const date = new Date();
     const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
 
-    try{
-        
-    }catch(err){
+    try {
+        const data = await User.aggregate([
+            { $match: { createdAt: { $gte: lastYear } } },
+            {
+                $project: {
+                    month: { $month: "$createdAt" },
+                },
+            },
+            {
+                $group:{
+                    _id: "$month",
+                    total: { $sum: 1},
+                }
+            }
+        ]);
+        res.status(200).json(data);
+    } catch (err) {
         res.status(500).json(err);
     }
-})
+});
 
 module.exports = router
