ea2f9e9e8cc595c9e70c2f71c78e3709b911851e
kennedy-anon <85305895+kennedy-anon@users.noreply.github.com>
2023-04-18 21:05:18 +0300

    sent product titles to algolia

--- routes/algolia.js
+++ routes/algolia.js
@@ -3,31 +3,29 @@ const algoliasearch = require('algoliasearch');
 const { verifyTokenAndAdmin } = require("./verifyToken");
 const Product = require("../models/Product");
 
-// Fetch data from database
-const fetchDataFromDatabase = async () => {
+
+// for sending product titles to algolia
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    // Fetch data from database
     const products = await Product.find({}, { _id: 1, title: 1 });
 
     // Map the products to Algolia objects
-    const algoliaObjects = products.map(product => ({
+    const records = products.map(product => ({
       objectID: product._id.toString(), // Set the objectID to the _id value from MongoDB
       title: product.title, // Other attributes of the product object
     }));
-
-    return algoliaObjects;
-
-}
-
-
-// for sending product titles to algolia
-router.post("/", verifyTokenAndAdmin, async (req, res) => {
-  try {
+    
     // Connect and authenticate with equickShop Algolia app
     const client = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_WRITE_KEY);
 
     // Create a new index and add records
     const index = client.initIndex('dev_equickShop');
-    const records = await fetchDataFromDatabase();
-    index.saveObject(records).wait();
+
+    for (let record in records) {
+      // console.log(records[record]);
+      await index.saveObject(records[record]); // sending records to algolia
+    }
 
     // Search the index and return the results
     index
